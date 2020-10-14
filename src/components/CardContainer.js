213356ba4d28ee85f41d8b317456c7e930515571
TJBachorz <TJBachorz@gmail.com>
2020-10-14 12:40:45 -0600

    for pull

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -8,31 +8,35 @@ export default class CardContainer extends Component {
         characters: [],
         randomCharacter: {}
     }
-    const [characters, setCharacters] = useState([])
-    const [randomCharacter, setRandomCharacter] = useState({})
 
-    useEffect(() => {
+    componentDidMount() {
         fetch("https://rickandmortyapi.com/api/character/")
             .then(response => response.json())
-            .then(data => setCharacters({characters: data.results}))
-    })
+            .then(data => this.setState({characters: data.results}))
+            .then(this.setState(
+                {randomCharacter: 
+                    this.state.characters[this.getRandomInt(this.state.characters.length)]
+                }
+            ))
+    }
 
-    const getRandomInt = (max) => {
+    getRandomInt = (max) => {
         return Math.floor(Math.random() * Math.floor(max));
     }
 
-    const makeRandomCharacter = () => {
-        let randomNumber = getRandomInt(characters.length)
-        let newRandomCharacter = characters[randomNumber]
-        setRandomCharacter({randomCharacter: newRandomCharacter})
+    // const makeRandomCharacter = () => {
+    //     let randomNumber = getRandomInt(characters.length)
+    //     let newRandomCharacter = characters[randomNumber]
+    //     setRandomCharacter({randomCharacter: newRandomCharacter})
         // return randomCharacter
         // return randomCharacter
         // return <Card character={randomCharacter}/>
+    // }
+    render() {
+        return (
+            <div>
+                <Card character={this.state.characters[1]}/>
+            </div>
+        )
     }
-
-    return (
-        <div onLoad={() => makeRandomCharacter}>
-            <Card character={randomCharacter}/>
-        </div>
-    )
 }
